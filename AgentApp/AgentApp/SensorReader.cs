e0270e7caa34540161de26eb56e2661d06415d94
Yurii Kulyk <yurii.kulyk@eleks.com>
2019-08-16 18:47:40 +0300

    Add project for pi Add classes to read cards

---
+++ AgentApp/AgentApp/SensorReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AgentApp.Wrapers;
+
+namespace AgentApp
+{
+    class SensorReader
+    {
+        public void Start()
+        {
+            var mfrc = new Mfrc522();
+
+            var task = new Task(async () => { await mfrc.InitIO(); });
+            task.RunSynchronously();
+
+
+            while (true)
+            {
+                if (mfrc.IsTagPresent())
+                {
+                    var uid = mfrc.ReadUid();
+
+                    mfrc.HaltTag();
+                }
+
+            }
+
+        }
+    }
+}
